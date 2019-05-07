f747fceea4e30e8152136fda99237f8a4484d7b0
JoaoCostaIFG <39132384+JoaoCostaIFG@users.noreply.github.com>
2019-05-07 02:35:06 +0000

    Add files via upload
    corrigir bugs de permissao de acesso e de syntax.
    agora temos overload travelpack++ e ++travelpack. fazem exatamente o mesmo but why not.
    adicionei um verify automatico na leitura para ver se e para ler landmarks ou so a destination principal (como eu disse num dos commits, pode n haver landmarks).
    temos um metodo que diz se este travelpack tem landmarks ou nao. temos um get destination (std::string) e um get landmarks (std::vector<std::string>).
    side note: *overall good shit. tmb corrigi path errors no meu windows mas isso n deve interessar falar sobre aqui.*
    lembrei-me de mais uma shit a meio de escrever a descricao por isso here we go:
    tinha um bug que o vector tinha 1 elemento a mais do que o necessario (uma string "" no fim). Outro bug era ler de um stream errada se houvessem landmarks (literalmente mudar de input_stream para input_string)
    tenho sono, vou dormir cy@

--- include/travelpack.h
+++ include/travelpack.h
@@ -13,17 +13,17 @@ class TravelPack{
         bool is_available();
         int get_id() const;
         std::string get_destination() const;
-        std::vector<std::string> get_land_marks() const;
+        bool has_landmarks();
+        std::vector<std::string> get_landmarks() const;
         std::string get_start_date() const;
         std::string get_end_date() const;
         float get_price() const;
-        unsigned int get_total_seats() const;
-        unsigned int get_sold_seats() const;
+        unsigned int get_seats() const;
     private:
         void rect_availability();
         int id;
         std::string destination;
-        std::vector<std::string> land_marks;
+        std::vector<std::string> landmarks;
         Date start_date;
         Date end_date;
         float price;
@@ -31,6 +31,7 @@ class TravelPack{
 };
 TravelPack::TravelPack(){
     id = 0x00000000;
+    //id = 10;
     destination = "";
     //start_date = Date();
     //end_date = Date();
@@ -42,12 +43,24 @@ TravelPack::TravelPack(std::istream &input_stream){
     std::istringstream input_string;
 
     input_stream >> id; input_stream.ignore(1000, '\n');
-    getline(input_stream, destination, '-'); destination = str_trim(destination);
-    getline(input_stream, temp_str); input_string.str(temp_str.append(" ,"));
-    while(!input_string.fail()){ //not sure if it works
-        getline(input_stream, temp_str, ','); temp_str = str_trim(temp_str);
-        land_marks.push_back(temp_str);
+
+    getline(input_stream, temp_str);
+    if (temp_str.find("-") != std::string::npos){ //verify if the land mark list is present
+        input_string.str(temp_str);
+        getline(input_string, destination, '-'); destination = str_trim(destination);
+        getline(input_string, temp_str); input_string.str(temp_str.append(" ,"));
+
+        while(!input_string.fail()){
+            getline(input_stream, temp_str, ','); temp_str = str_trim(temp_str);
+            if (temp_str.length() != 0)
+                landmarks.push_back(temp_str);
+        }
+
+    }else{
+        destination = str_trim(temp_str);
     }
+    
+
 
     getline(input_stream, temp_str); input_string.str(temp_str.append(" /"));
     start_date = Date(input_string);
@@ -67,7 +80,6 @@ TravelPack::TravelPack(std::istream &input_stream){
 
 
 void TravelPack::print(std::ostream &output_stream){
-
 }
 
 
@@ -109,8 +121,13 @@ int TravelPack::get_id() const{
 std::string TravelPack::get_destination() const{
     return destination;
 }
-std::vector<std::string> TravelPack::get_land_marks() const{
-    return land_marks;
+bool TravelPack::has_landmarks(){
+    if (landmarks.size() == 0)
+        return false;
+    return true;
+}
+std::vector<std::string> TravelPack::get_landmarks() const{
+    return landmarks;
 }
 std::string TravelPack::get_start_date() const{
     return std::to_string(start_date.get_day()) + "/" + std::to_string(start_date.get_month()) + "/" + std::to_string(start_date.get_year());
@@ -121,16 +138,16 @@ std::string TravelPack::get_end_date() const{
 float TravelPack::get_price() const{
     return price;
 }
-unsigned int TravelPack::get_total_seats() const{
-    return total_seats;
-}
-unsigned int TravelPack::get_sold_seats() const{
-    return sold_seats;
+unsigned int TravelPack::get_seats() const{
+    return seats;
 }
 
 
 //Operator overload
-void operator++(TravelPack &travelpack){
+void operator++(TravelPack &travelpack){ //++travelpack
+    travelpack.buy_seat();
+}
+void operator++(TravelPack &travelpack, int){ //travelpack++
     travelpack.buy_seat();
 }
 
